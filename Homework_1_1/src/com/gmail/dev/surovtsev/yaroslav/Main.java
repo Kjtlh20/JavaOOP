f651f2b0e6a9e295de089f99c8e6252aa72c9924
Yaroslav Surovtsev <yaroslav.surovtsev.dev@gmail.com>
2021-08-17 22:19:38 +0300

    did homework 1.1

--- Homework_1_1/src/com/gmail/dev/surovtsev/yaroslav/Main.java
+++ Homework_1_1/src/com/gmail/dev/surovtsev/yaroslav/Main.java
@@ -6,6 +6,7 @@
 package com.gmail.dev.surovtsev.yaroslav;
 
 public class Main {
+
     public static void main(String[] args) {
         Goods goods1 = new Goods(1, "iPhone 7 Plus 256GB", "", 500.50, 0.1, "5901234123457", "MWLK2", true);
         Goods goods2 = new Goods(2, "iPhone 8 64GB", "", 400.80, 0.09, "59018764123457", "MHDH3", true);
